9a201f0eb918296120cdc71455b583ca948a6e4a
ppic2 <ppic2@pkshp.com>
2026-08-20 15:57:47 +0700

    perbarui seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,59 +27,15 @@ class DatabaseSeeder extends Seeder
         $ppicRole->syncPermissions([$accessPlanning, $accessExecution]);
         $spvRole->syncPermissions([$accessExecution]);
 
-        $adminUser = User::factory()->create([
-            'name' => 'Admin PPIC',
-            'email' => 'adminppicpf@peroniks.com',
-            'password' => bcrypt('password'),
-        ]);
-        $adminUser->assignRole($adminRole);
-
-        User::factory()->create([
-            'name' => 'MR Peroniks',
-            'email' => 'mr@peroniks.com',
-            'password' => bcrypt('password123'),
-        ]);
+        $this->seedUser('Admin PPIC', 'adminppicpf@peroniks.com', 'password', 'admin');
+        $this->seedUser('MR Peroniks', 'mr@peroniks.com', 'password123');
 
         // Seed new RBAC / product scope users
-        $u1 = User::factory()->create([
-            'name' => 'PPIC Flange',
-            'email' => 'ppicflange@peroniks.com',
-            'password' => bcrypt('password'),
-            'product_scope' => 'FLANGE_STAINLESS',
-        ]);
-        $u1->assignRole($ppicRole);
-
-        $u2 = User::factory()->create([
-            'name' => 'PPIC Flange Besi',
-            'email' => 'ppicflangebesi@peroniks.com',
-            'password' => bcrypt('password'),
-            'product_scope' => 'FLANGE_BESI',
-        ]);
-        $u2->assignRole($ppicRole);
-
-        $u3 = User::factory()->create([
-            'name' => 'PPIC Fitting',
-            'email' => 'ppicfitting@peroniks.com',
-            'password' => bcrypt('password'),
-            'product_scope' => 'FITTING_STAINLESS',
-        ]);
-        $u3->assignRole($ppicRole);
-
-        $u4 = User::factory()->create([
-            'name' => 'Admin Fitting',
-            'email' => 'adminfitting@peroniks.com',
-            'password' => bcrypt('password'),
-            'product_scope' => null,
-        ]);
-        $u4->assignRole($adminRole);
-
-        $u5 = User::factory()->create([
-            'name' => 'SPV Lapisan',
-            'email' => 'spvlapisan@peroniks.com',
-            'password' => bcrypt('password'),
-            'product_scope' => null,
-        ]);
-        $u5->assignRole($spvRole);
+        $this->seedUser('PPIC Flange', 'ppicflange@peroniks.com', 'password', 'ppic', 'FLANGE_STAINLESS');
+        $this->seedUser('PPIC Flange Besi', 'ppicflangebesi@peroniks.com', 'password', 'ppic', 'FLANGE_BESI');
+        $this->seedUser('PPIC Fitting', 'ppicfitting@peroniks.com', 'password', 'ppic', 'FITTING_STAINLESS');
+        $this->seedUser('Admin Fitting', 'adminfitting@peroniks.com', 'password', 'admin');
+        $this->seedUser('SPV Lapisan', 'spvlapisan@peroniks.com', 'password', 'spv');
 
         $this->call([
             CustomerSeeder::class,
@@ -89,6 +45,35 @@ class DatabaseSeeder extends Seeder
             $this->call([
                 ProductionDummySeeder::class,
             ]);
+        }
+    }
+
+    /**
+     * Seed or update a user idempotently.
+     */
+    private function seedUser(string $name, string $email, string $defaultPassword, ?string $role = null, ?string $productScope = null): void
+    {
+        $user = User::where('email', $email)->first();
+
+        if ($user) {
+            // User exists: update name and product scope, but preserve password
+            $user->update([
+                'name' => $name,
+                'product_scope' => $productScope,
+            ]);
+        } else {
+            // User does not exist: create user with default password
+            $user = User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => bcrypt($defaultPassword),
+                'product_scope' => $productScope,
+            ]);
+        }
+
+        // Sync Spatie role if provided
+        if ($role) {
+            $user->syncRoles([$role]);
         }
     }
 }
